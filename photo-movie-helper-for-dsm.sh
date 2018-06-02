acc041ba71061d198eb74b56e1ab94e9a3258e8b
Dave Weniger <daversion@gmail.com>
2018-06-01 21:21:22 -0700

    Update photo-movie-helper-for-dsm.sh

--- photo-movie-helper-for-dsm.sh
+++ photo-movie-helper-for-dsm.sh
@@ -25,7 +25,7 @@
 LOG=/volume1/general/Dropbox/photo-movie-helper.log
 
 # Input directory where imported files can be found
-INPUT_FOLDER=/volume1/general/Dropbox/Photos/CopyTest
+INPUT_FOLDER="/volume1/general/Dropbox/Photos/CopyTest"
 
 SD_COPY_FOLDER_PREFIX=TempSort
 
